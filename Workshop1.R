2f1dc13554b4f53bccf0d4b5dc03c3b37d104b7b
anastasia9624 <a.ivanova@se20.qmul.ac.uk>
2023-02-18 16:33:31 +0000

    Finished Workshop 1 by completing sections 6 and 7.

--- Workshop1.R
+++ Workshop1.R
@@ -46,7 +46,6 @@ z <- FALSE
 z == TRUE #Q4
 
 # 4.2. Challenge time
-
 # Q5) Make an object with the value 'The cake' and another with the value 'A lie'. 
 # Create a logical expression to test whether these two values are the same.
 cake <- 'The cake'
@@ -104,7 +103,7 @@ x[6]
 x[2:7]
 x[1:4]
 x[seq(from = 1, to = 7, by = 3)]
-# subscript vectors (or matrices ordataframes) using logical statements:
+# subscript vectors (or matrices or dataframes) using logical statements:
 x <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1)
 x > 5
 y <- x[x > 5]
@@ -152,10 +151,69 @@ alp
 # 6. The Dataframe
 # 6.1. The basics
 
-# 6.2. The $ - NOT FINISHED
-df <- read.csv('NYTBestsellers.scv')
-head(df)
-df$number_of_pages <- rep(c(100,200,300,400,500), each = 20)
+df <- read.csv("NYTBestsellers.csv") # Read the NYTBestsellers.csv dataset into R
+# df <- read.csv('NYTBestsellers.csv') # this also would work
+str(df) # Check the structure of the dataframe
+df[1:3, 2:5] # subscript data in a dataframe
+row3 <- df[3,] # get the information from row 3 using a subscript
+print(row3)
+first5 <- df[1:5, 'title'] # access the first five elements of the title column using a subscript
+print(first5)
+
+
+# 6.2. The $ 
+# $ symbol is used to refer to columns in a dataframe
+byweeks <- hist(df$total_weeks) # make a histogram of frequency by weeks 
+
+df[1:5, 'title'] # subscript a single column using the square brackets
+df$title[1:5] # another way, gives the same result
+
+print(df)
+df[70:80, 'author'] 
+
+# create a new column called personal_rating and fill it with the term ‘brilliant’
+df$personal_rating <- rep('brilliant', times = nrow(df)) 
+
+# Try creating your own new column called ‘number_of_pages’ that creates a column with 100 
+# for the first 20 books, 200 for the next 20 and so on until the last 20 books are 500.
+df$number_of_pages <- rep(c(100,200,300,400,500), each = 20) 
+
+# Adapt your previous answer to make use of the seq() function to create the input for the rep() function.
 df$number_of_pages <- rep(seq(100,500, by = 100), each = 20)
-df$number_of_pages
+print(df) 
+
+# 7. Question Time 2
+# Q8) What type and class are each of objects x, y and z in the following code:
+w <- 'melanogaster' # character
+x <- 1:12 #  numeric / integer 
+y <- FALSE # logical
+z <- x + 1 # numeric / integer
+class(z)
+typeof(z)
+
+# Q9) Create a vector of every even number between 1 and 100.
+vec <- seq(from = 2, to = 100, by = 2)
+
+# Q10) Create a vector of all the odd numbers between 1000 and 1500 that are divisible by 7.
+1000/7 #143
+7*143 #1001
+oddvec <- seq(from = 1001, to = 1500, by = 2) # odd numbers 
+oddvec <- oddvec[oddvec %% 7 == 0] # Remainder of division by 7 is 0
+
+# Q11) Using the NYT bestseller data, create a vector of the names all the books that were in the top 
+# spot for at least 10 weeks.
+weeks10 <- df[df$total_weeks >= 10, 'title'] # or df$title[df$total_weeks >= 10]
+
+# Q12) Using the NYT bestseller data, create a new dataframe where all the books were in the top spot for 
+# less than 10 weeks and have titles that are less than 15 characters long.
+newdf <- df[df$total_weeks < 10 & nchar(df$title) < 15,]
+
+# Q13) Add a new column to your dataframe called ‘long10’ that contains the word ‘yes’ if the book 
+# title is 10 characters long, and ‘no’ if it is not.
+newdf$long10 <- 'yes'
+newdf$long10[nchar(newdf$title) != 10] <- 'no'
+
+# Q14) Using subscripting, update your edited dataframe to only contain 
+# the ‘title’, ‘author’, ‘year’ and ‘long10’ columns.
+finaldf <- newdf[ , c('title', 'author', 'year', 'long10')]
 
